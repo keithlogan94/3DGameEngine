a882d8f35ab3dec13525d00fe43bc66bc7cf2e88
Keith Becker <keithloganbecker94@gmail.com>
2017-05-03 12:00:48 -0600

    Initial Updates

--- Main.cpp
+++ Main.cpp
@@ -5,17 +5,56 @@
 // Software By Keith Becker Copyright 2017 (c)
 //
 
-#include <iostream>
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 
-int main(void) {
+#include <iostream>
+#include <stdexcept>
+#include <exception>
+
+int main(int argc, char ** argv) {
+	//Initialize GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+	//Create Window
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (!window) {
+		const char *err = "The Window failed to create.";
+		std::cerr << err;
+		throw std::exception{ err };
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = GL_TRUE;
+	
+	//Initialize Glew
+	if (glewInit() != GLEW_OK) {
+		const char *err = "Failed to initialize glew";
+		std::cerr << err;
+		throw std::exception{ err };
+		//delete window...
 
+		glfwTerminate();
+		return -1;
+	}
+
+	//Set the view port for rendering
+	int width = 800, height = 600;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	//Game Loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	//Clean Up
+	glfwTerminate();
 	return 0;
 }
